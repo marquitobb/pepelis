1d4e5936287b7f43773f2bf7186c17ad4d77e70a
marquitobb <42453766+marquitobb@users.noreply.github.com>
2019-05-19 13:56:16 -0500

    la base de datos esta pa nomas agregarla

--- pepelis.sql
+++ pepelis.sql
@@ -1,32 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 13-05-2019 a las 14:13:25
--- Versión del servidor: 10.1.38-MariaDB
--- Versión de PHP: 7.3.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `pepelis`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `peliculas`
---
+create DATABASE pepelis;
+USE pepelis;
 
 CREATE TABLE `peliculas` (
   `idpeliculas` int(11) NOT NULL,
